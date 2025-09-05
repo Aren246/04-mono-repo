b9ec16ba6e62ae2c778c72fd53e736ef82db00d5
ARENVIR BOHGAN <ASBOHGAN@GMAIL.COM>
2025-09-05 16:51:49 +0100

    updated app.js to hopefully complete assignment

--- client/app.js
+++ client/app.js
@@ -1,34 +1,37 @@
 const form = document.getElementById("guestForm");
 const messagesDiv = document.getElementById("messages");
 
-const loadMessages = async () => {
-    const res = await fetch("http://localhost:8080/api/messages");
-    const data = await res.json();
-}
 
-messagesDiv.Div.innerHTML = "";
-data.forEach(m => {
+const API = "https://zero4-mono-repo-n7n0.onrender.com";
+
+const loadMessages = async () => {
+  const res = await fetch(`${API}/api/messages`);
+  const data = await res.json();
+
+
+
+  data.forEach(m => {
     const div = document.createElement("div");
     div.className = "msg";
     div.textContent = `${m.name}: ${m.message}`;
     messagesDiv.appendChild(div);
-})
+  });
+};
 
 form.addEventListener("submit", async e => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const name = document.getElementById("name").value;
-    const message = document.getElementById("message").value;
+  const name = document.getElementById("name").value;
+  const message = document.getElementById("message").value;
 
-
-await fetch("http://localhost:8080/api/messages", {
+  await fetch(`${API}/api/messages`, {
     method: "POST",
-    headers: { "Content-Type": "application/json"},
-    body: JSON.stringify({name, message})
-});
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ name, message })
+  });
 
-form.reset();
-loadMessages();
+  form.reset();
+  loadMessages();
 });
 
 loadMessages();
